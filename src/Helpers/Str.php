26140eeb1a070b40880d557bb9d269a99f57e2a1
david_smith <david_smith@sweetwater.com>
2024-08-30 08:57:26 -0400

    Update `Str.php`

--- src/Helpers/Str.php
+++ src/Helpers/Str.php
@@ -2,16 +2,24 @@
 
 namespace Zerotoprod\DataModel\Helpers;
 
+/**
+ * The `Str` helper class defines constants for pattern matching and type identification
+ * within the DataModel package, aiding in parsing and handling type annotations in doc comments.
+ *
+ * @package Zerotoprod\DataModel\Helpers
+ */
 class Str
 {
-    public const pattern = '/@var\s+(?P<type>int|float|null|never|void|enum|bool|true|false|string|array|iterable|object|stdClass|\\\stdClass|mixed|resource|numeric|[\w\\\\]+\|[\w\\\\]+)?\s*(?P<class_type>[\\\\\w]+)?/';
-    public const class_type = 'class_type';
-    public const from = 'from';
+    public const pattern = '/@var\s+(?P<native_type>string|array|int|bool|object|float|stdClass|\\\stdClass|[\w\\\\]+\|[\w\\\\]+)?\s*(?P<type>[\\\\\w]+)?/';
     public const type = 'type';
+    public const from = 'from';
+    public const native_type = 'native_type';
     public const string = 'string';
     public const array = 'array';
     public const int = 'int';
     public const bool = 'bool';
     public const object = 'object';
     public const float = 'float';
+    public const stdClass = 'stdClass';
+    public const _stdClass = '\stdClass';
 }
