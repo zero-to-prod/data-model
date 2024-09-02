a3ec7a308f383e56351c74d4221f120b68a09f55
david_smith <david_smith@sweetwater.com>
2024-09-02 08:20:50 -0400

    Update tests

--- tests/Unit/FromDataModel/BaseClass.php
+++ tests/Unit/FromDataModel/BaseClass.php
@@ -8,8 +8,13 @@ class BaseClass
 {
     use DataModel;
 
+    public const id = 'id';
     public const Child = 'Child';
-
+    public const NullChild = 'NullChild';
+    /** @var int $id */
+    public $id;
     /** @var Child $Child */
     public $Child;
+    /** @var Child $NullChild */
+    public $NullChild;
 }
