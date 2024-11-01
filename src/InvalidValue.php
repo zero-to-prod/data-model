89dd68a4eaeca84162beac1e3f469b8ba7e9e416
david_smith <david_smith@sweetwater.com>
2024-11-01 08:39:13 -0400

    Create `InvalidValue.php`.

---
+++ src/InvalidValue.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Zerotoprod\DataModel;
+
+use RuntimeException;
+
+/**
+ * Thrown when a property is required.
+ *
+ * @link https://github.com/zero-to-prod/data-model
+ * @see  https://github.com/zero-to-prod/data-model-helper
+ * @see  https://github.com/zero-to-prod/data-model-factory
+ * @see  https://github.com/zero-to-prod/transformable
+ */
+class InvalidValue extends RuntimeException
+{
+
+}
